9a9d553c0323b2bc176e539e38335d78eb59509b
doprz <52579214+doprz@users.noreply.github.com>
2026-03-06 20:28:43 -0600

    feat(env): add T3 Env

--- src/server/db/init.ts
+++ src/server/db/init.ts
@@ -1,5 +1,6 @@
 import "dotenv/config";
 import { drizzle } from "drizzle-orm/node-postgres";
+import { env } from "@/env";
 import * as authSchema from "@/server/db/auth-schema";
 import * as apiSchema from "@/server/db/schema";
 
@@ -8,4 +9,4 @@ const schema = {
   ...authSchema,
 };
 
-export const db = drizzle(process.env.DATABASE_URL!, { schema });
+export const db = drizzle(env.DATABASE_URL, { schema });
